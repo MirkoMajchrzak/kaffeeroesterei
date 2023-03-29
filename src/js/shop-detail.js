44a62b5f743f5017fb0123af1ad2706c6446c420
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-03-29 20:39:14 +0200

    shop-detail.js tuned

--- src/js/shop-detail.js
+++ src/js/shop-detail.js
@@ -1,3 +1,5 @@
+const appContainer = document.querySelector(".shop-detail");
+// load everything into this classnamed container
 async function getProductFromUrl() {
     // fetch all products from json
     const response = await fetch("../shop/shop.json");
@@ -11,7 +13,24 @@ async function getProductFromUrl() {
             return product;
         }
     })[0];
+    console.log(product);
     // render product in detail page
+    const productHTML = `
+    <div class="detail-image">
+          <img src="../images/FEND_Coffee_Costa-Rica-2.png" alt="Kaffee aus Costa Rica" class="detail-image__img">
+        </div>
+        
+        <section class="detail-info">
+          <div class="detail-info__text">
+            <h2 class="h2__m">${product.productName}</h2>
+            <p class="subhead__m--black">${product.price}</p>
+            <p class="body-text__m">${product.description}</p>
+
+          </div>
+          </section>
+    `;
+  console.log(products);
+  appContainer.innerHTML = productHTML;
 };
 
-fetchProducts();
+getProductFromUrl();
