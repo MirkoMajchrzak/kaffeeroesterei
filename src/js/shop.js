6365e474fe3d8bec1ee2e71cf9e3394a5acb3209
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-03-30 20:19:58 +0200

    added toFixed to shop.js

--- src/js/shop.js
+++ src/js/shop.js
@@ -16,7 +16,7 @@ async function fetchProducts() {
               <img class="product__image" src="../images/FEND_Coffee_Costa-Rica-2.png" alt="Kaffee aus Costa Rica">
             </div>
             <p class="bodytext__m">${product.productName}</p>
-            <p class="bodytext__m--grey">${product.price}</p>
+            <p class="bodytext__m--grey">${((product.price / 100).toFixed(2)) +(" €") }</p>
             <img src="../images/icon_kaffee_bohnen.svg" alt="Kaffeebohnen" class="product__icons">
             <img src="../images/icon_frenchpress.svg" alt="Frenchpress" class="product__icons">
             <img src="../images/icon_kaffee_schaufel.svg" alt="Kaffeeschaufel" class="product__icons">
