18b38e8211b2d7b338174321b67ecbc7baf19e1b
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-04-20 17:20:05 +0200

    comment in shopping cart added

--- src/js/shopping-cart.js
+++ src/js/shopping-cart.js
@@ -1,22 +1,21 @@
 const openCart = () => {
-    const cartDiv = document.createElement ("div");
-    cartDiv.classList.add("cartdiv");
-    document.body.append(cartDiv);
-    const templateCart = `
-    <h2>Test</h2>
+  const cartDiv = document.createElement("div");
+  cartDiv.classList.add("cartdiv");
+  document.body.append(cartDiv);
+  const templateCart = `
+    <h2>Warenkorb</h2>
     <span class"span">Dies ist ein Test</span>
     <button class="closeBtn" id="closeCart">X</button>`;
-    const closeCart = () => {
-        cartDiv.remove("cartdiv");
-    };
+  // 2 divs
+  const closeCart = () => {
+    cartDiv.remove("cartdiv");
+  };
 
-    cartDiv.innerHTML = templateCart;
-    const closeBtn = document.querySelector(".closeBtn");
-    closeBtn.addEventListener("click", closeCart)
-}
+  cartDiv.innerHTML = templateCart;
+  const closeBtn = document.querySelector(".closeBtn");
+  closeBtn.addEventListener("click", closeCart);
+};
 
-
-//Warenkorb ansprechen
+// Warenkorb ansprechen
 const cartBtn = document.querySelector(".cartBtn");
 cartBtn.addEventListener("click", openCart);
-
